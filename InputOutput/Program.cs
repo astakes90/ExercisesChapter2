3880160aec67026189884cb2b236aed8f6e76403
Annalyssa Stakes <annalyssastakes@Annalyssas-Air.attlocal.net>
2022-08-01 17:11:37 -0500

    first exercise done

---
+++ InputOutput/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Console.WriteLine("What is your name?");
+string name = Console.ReadLine();
+Console.WriteLine("Hello " + name);
